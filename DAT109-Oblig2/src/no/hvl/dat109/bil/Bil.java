71e6c1d473905cc0bed3ed3722b19b5c897220db
cerberus <cerberus@DESKTOP-LUJ2O9O.lan>
2021-02-11 15:40:13 +0100

    Oppdatert bil

--- DAT109-Oblig2/src/no/hvl/dat109/bil/Bil.java
+++ DAT109-Oblig2/src/no/hvl/dat109/bil/Bil.java
@@ -36,6 +36,20 @@ public class Bil {
 
 	}
 
+	/**
+	 * Tar inn en bil og returner Returnerer stringverdier for web
+	 * 
+	 * 
+	 * 
+	 * @return
+	 */
+
+	public String utskriftLedigBilWeb(Bil bil) {
+
+		return "Fredrik fikser";
+
+	}
+
 	@Override
 	public String toString() {
 		return "Bil [regnr=" + regnr + ", merke=" + merke + ", modell=" + modell + ", farge=" + farge + ", gruppe="
@@ -81,6 +95,5 @@ public class Bil {
 	public void setGruppe(String gruppe) {
 		this.gruppe = gruppe;
 	}
-
 
 }
