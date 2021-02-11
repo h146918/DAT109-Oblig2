f603c1944ab57c1309e41c365f8f42d136ba52f2
fredrikvalset <53392485+fredrikvalset@users.noreply.github.com>
2021-02-11 14:19:21 +0100

    Laget reserver.jsp og servletinfo

---
+++ DAT109-Oblig2/WebContent/WEB-INF/reserver.jsp
@@ -0,0 +1,42 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+
+	<form id="reserver" action="ReserverServlet" method="post">
+	<fieldset>
+			<div>
+				<label for="fornavn">Fornavn:</label> 
+				<input id="fornavn" type="text"
+					name="fornavn" value="${fornavn}"  required/> 
+					<font color="red" >${feilFornavn}</font>
+			</div>
+			<div class="pure-control-group">
+				<label for="etternavn">Etternavn:</label> <input id="etternavn" type="text"
+					name="etternavn" value="${etternavn}" required/> 
+					<font color="red">${feilEtternavn}</font>
+			</div>
+			<div class="pure-control-group">
+				<label for="adresse">Adresse:</label> <input id="adresse" type="text"
+					name="adresse" value="${adresse}" required /> 
+					<font color="red">${feilAdresse}</font>
+			</div>
+			<div>
+				<label for="telefon">Telefon:</label> <input id="telfon" type="text"
+					name="passord" value="${telefon}" required /> 
+					<font color="red">${feilTelefon}</font>
+			</div>
+		
+			<div class="pure-controls">
+				<button id="btn" type="submit">Send inn</button>
+			</div>
+		</fieldset>
+	</form>
+
+</body>
+</html>
--- DAT109-Oblig2/src/no/hvl/dat109/servlets/ReserverServlet.java
+++ DAT109-Oblig2/src/no/hvl/dat109/servlets/ReserverServlet.java
@@ -1,4 +1,4 @@
-package no.hvl.dat109.Servlets;
+package no.hvl.dat109.servlets;
 
 import java.io.IOException;
 import javax.servlet.ServletException;
@@ -7,27 +7,35 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class reserverServlet
- */
-@WebServlet("/reserverServlet")
-public class reserverServlet extends HttpServlet {
+
+@WebServlet("/ReserverServlet")
+public class ReserverServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
  
-    }
+   
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	
+		request.getRequestDispatcher("WEB-INF/reserver.jsp").forward(request, response);
+		
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
+	
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		String fornavn = request.getParameter("fornavn");
+		String etternavn = request.getParameter("etternavn");
+		String adresse = request.getParameter("adresse");
+		String telefonString = request.getParameter("telefon");
+		
+		Integer telefon = Integer.parseInt(telefonString);
+		
+		
+		
 	
 	}
 
