5744175c359da254dac853ecaa1042556e2b6076
daniel <daniel@DESKTOP-L1LJJDN>
2021-02-11 13:28:22 +0100

    test

--- DAT109-Oblig2/src/no/hvl/dat109/Servlets/reserverServlet.java
+++ DAT109-Oblig2/src/no/hvl/dat109/Servlets/reserverServlet.java
@@ -14,28 +14,21 @@ import javax.servlet.http.HttpServletResponse;
 public class reserverServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public reserverServlet() {
-        super();
-        // TODO Auto-generated constructor stub
+ 
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	
 	}
 
 }
